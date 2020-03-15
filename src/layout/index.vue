8e1e2fb1080162eef8bcd5519812fb75309faeb1
郝星 <haoxanchent.com>
2020-03-15 12:34:35 +0800

    update sidebar

--- src/layout/index.vue
+++ src/layout/index.vue
@@ -1,10 +1,12 @@
 <template>
-  <div class="app-container">
+  <div :class="['app-container', {isCollaseSidebar: isCollaseSidebar}]">
     <Sidebar class="sidebar-container" />
 
     <div class="main-container">
       <Navbar class="navbar-container" />
-      <router-view></router-view>
+      <section class="content-container">
+        <router-view :key="key"></router-view>
+      </section>
     </div>
   </div>
 </template>
@@ -15,7 +17,16 @@ import { Sidebar, Navbar } from './components'
 export default {
   name: 'Layout',
 
-  components: { Sidebar, Navbar }
+  components: { Sidebar, Navbar },
+
+  computed: {
+    key () {
+      return this.$route.path
+    },
+    isCollaseSidebar () {
+      return this.$store.state.app.isCollaseSidebar
+    }
+  }
 }
 </script>
 
--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -58,7 +58,7 @@ export default {
 }
 </script>
 
-<style scoped lang="scss">
+<style lang="scss">
 .login-container {
   .title {
     font-size: 36px;
